af1bda544176f38ab8df8a22cf43e91d9b2efb57
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@a7d08680-b437-11de-9e28-3f17fa8be27d>
2009-10-15 14:37:35 +0000

    Add statemachine for single/double/long tapping

--- widgets/qgaugewidget.cpp
+++ widgets/qgaugewidget.cpp
@@ -1,25 +1,39 @@
 #include <QtGui>
 #include "qgaugewidget.h"
 #include <iostream.h>
-
+/*
+static const int QGaugeWidget::StIdle = 0;
+static const int QGaugeWidget::StInitialTouch = 1;
+static const int QGaugeWidget::StSingleTapPending = 2;
+static const int QGaugeWidget::StLongTouch = 3;
+static const int QGaugeWidget::StSecondTouch = 4;
+static const int QGaugeWidget::StMoving = 5;
+*/
 QGaugeWidget::QGaugeWidget(QWidget *parent)
     : QWidget(parent)
-    , longpress(false)
-    , timer(0)
+    //, longpress(false)
+    //, timer(0)
 {
-    timer = new QTimer(this);
-    timer->setSingleShot(true);
-    timer->setInterval(1000);
-    connect(timer,SIGNAL(timeout()),this,SLOT(timerExpired()));
+    //timer = new QTimer(this);
+    //timer->setSingleShot(true);
+    //timer->setInterval(1000);
+    //connect(timer,SIGNAL(timeout()),this,SLOT(timerExpired()));
+
+	singletaptimer.setSingleShot(true);
+	singletaptimer.setInterval(300);
+	connect(&singletaptimer,SIGNAL(timeout()),this,SLOT(singleTapTimeout()));
+	longtaptimer.setSingleShot(true);
+	longtaptimer.setInterval(1000);
+	connect(&longtaptimer,SIGNAL(timeout()),this,SLOT(longTapTimeout()));
 }
 
 QGaugeWidget::~QGaugeWidget()
 {
-    cancelTimer();
-    delete timer;
-    timer =0;
+    //cancelTimer();
+    //delete timer;
+    //timer =0;
 }
-
+/*
 void QGaugeWidget::startTimer()
 {
     //cout << "Gauge timer started\n";
@@ -36,16 +50,86 @@ void QGaugeWidget::timerExpired()
     //cout << "Gauge timer expired\n";
     longpress = true;
 }
-
-void QGaugeWidget::mousePressEvent(QMouseEvent *event)
+*/
+void QGaugeWidget::mousePressEvent(QMouseEvent * /* event */)
 {
     //cout << "Gauge mouse press\n";
-    longpress = false;
-    startTimer();
+    //longpress = false;
+    //startTimer();
+
+	switch(state)
+	{
+	case StIdle:
+		startLongTapTimer();
+		state = StInitialTouch;
+		break;
+	case StSingleTapPending:
+		cancelSingleTapTimer();
+		startLongTapTimer();
+		state = StSecondTouch;
+		break;
+	default:
+		// error!
+		break;
+	}
 }
 
-void QGaugeWidget::mouseReleaseEvent(QMouseEvent *event)
+void QGaugeWidget::longTapTimeout()
 {
+	switch(state)
+	{
+	case StInitialTouch:
+	case StSecondTouch:
+		state = StLongTouch;
+		break;
+	default:
+		// error!
+		break;
+	}
+}
+
+void QGaugeWidget::singleTapTimeout()
+{
+	switch(state)
+	{
+	case StSingleTapPending:
+		state = StIdle;
+		emit singleTap();
+		break;
+	default:
+		// error!
+		break;
+	}
+}
+
+void QGaugeWidget::startLongTapTimer()
+{
+    longtaptimer.start();
+}
+
+void QGaugeWidget::startSingleTapTimer()
+{
+	singletaptimer.start();
+}
+
+void QGaugeWidget::cancelLongTapTimer()
+{
+	longtaptimer.stop();
+}
+
+void QGaugeWidget::cancelSingleTapTimer()
+{
+	singletaptimer.stop();
+}
+
+void QGaugeWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    // if (movement not too small) emit drag event
+}
+
+void QGaugeWidget::mouseReleaseEvent(QMouseEvent * /*event*/)
+{
+/*
     //cout << "Gauge mouse release\n";
     if (longpress)
     {
@@ -58,4 +142,27 @@ void QGaugeWidget::mouseReleaseEvent(QMouseEvent *event)
         emit zoom();
     }
     longpress = false;
+*/
+	switch(state)
+	{
+	case QGaugeWidget::StInitialTouch:
+		cancelLongTapTimer();
+		state = StSingleTapPending;
+		break;
+	case QGaugeWidget::StSecondTouch:
+		cancelLongTapTimer();
+		state = StIdle;
+		emit doubleTap();
+		break;
+	case QGaugeWidget::StLongTouch:
+		state = StIdle;
+		emit longTap();
+		break;
+	case QGaugeWidget::StMoving:
+		// emit drag event
+		break;
+	default:
+		// error!
+		break;
+	}
 }
--- widgets/qgaugewidget.h
+++ widgets/qgaugewidget.h
@@ -2,6 +2,7 @@
 #define QGAUGEWIDGET_H
 
 #include <QWidget>
+#include <QTimer>
 
 class QGaugeWidget : public QWidget
 {
@@ -11,21 +12,45 @@ public:
     ~QGaugeWidget();
 
 signals:
-    void zoom();
-    void options();
+    //void zoom();
+    //void options();
+    
+    void singleTap();
+    void doubleTap();
+    void longTap();
+    void drag(QPoint delta);
 
 protected slots:
-    void timerExpired();
+    //void timerExpired();
+    void longTapTimeout();
+    void singleTapTimeout();
 
 protected:
-    void cancelTimer();
-    void startTimer();
+    //void cancelTimer();
+    //void startTimer();
     virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseMoveEvent(QMouseEvent *event);
     virtual void mouseReleaseEvent(QMouseEvent *event);
+    void QGaugeWidget::startLongTapTimer();
+    void QGaugeWidget::cancelLongTapTimer();
+    void QGaugeWidget::startSingleTapTimer();
+    void QGaugeWidget::cancelSingleTapTimer();
 
 private:
-    QTimer *timer;
-    bool longpress;
+	
+	static const int StIdle = 0;
+	static const int StInitialTouch = 1;
+	static const int StSingleTapPending = 2;
+	static const int StLongTouch = 3;
+	static const int StSecondTouch = 4;
+	static const int StMoving = 5;
+	
+    QTimer longtaptimer;
+    QTimer singletaptimer;
+    QPoint previous;
+    int state;
+    //QTimer *timer;
+    //bool longpress;
 };
 
 #endif // QGAUGEWIDGET_H
