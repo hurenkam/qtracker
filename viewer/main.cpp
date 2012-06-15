d46004597692150fc128d80e7781b161f95971dc
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@a7d08680-b437-11de-9e28-3f17fa8be27d>
2012-06-15 23:55:35 +0000

    updates to get new viewer working

--- viewer/main.cpp
+++ viewer/main.cpp
@@ -1,11 +1,32 @@
 #include <QtGui/QApplication>
+#include <QtDeclarative>
+#include <QDeclarativeEngine>
 #include "qmlapplicationviewer.h"
+
+#include "qmldatabase.h"
+
+void registerTypes()
+{
+//    qmlRegisterType<QDeclarativeFolderListModel>("QmlTrackerExtensions",1,0,"FolderListModel");
+//    qmlRegisterType<DeviceInfoModel>("QmlTrackerExtensions",1,0,"DeviceInfoModel");
+//    qmlRegisterType<MapView>("QmlTrackerExtensions",1,0,"MapView2");
+//    qmlRegisterType<GpxFile>("QmlTrackerExtensions",1,0,"GpxFile");
+    qmlRegisterType<qmlDatabase>("QmlTrackerExtensions",1,0,"TDatabase");
+    qmlRegisterType<qmlCategory>("QmlTrackerExtensions",1,0,"TCategory");
+    qmlRegisterType<qmlMap>("QmlTrackerExtensions",1,0,"TMap");
+    qmlRegisterType<qmlTrip>("QmlTrackerExtensions",1,0,"TTrip");
+    qmlRegisterType<qmlWaypoint>("QmlTrackerExtensions",1,0,"TWaypoint");
+    qmlRegisterType<qmlRoute>("QmlTrackerExtensions",1,0,"TRoute");
+    qmlRegisterType<qmlTrack>("QmlTrackerExtensions",1,0,"TTrack");
+    qmlRegisterType<qmlRefpoint>("QmlTrackerExtensions",1,0,"TRefpoint");
+}
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
     QScopedPointer<QmlApplicationViewer> viewer(QmlApplicationViewer::create());
 
+    registerTypes();
 #if !defined(Q_OS_ANDROID)
     viewer->setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer->setMainQmlFile(QLatin1String("main/Main.qml"));
